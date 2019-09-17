2e62409ddc0f96cc84053a7efdfa8007dbd4f676
Will Wheeler <willfulbard@gmail.com>
2019-09-16 20:23:48 -0700

    Create very basic form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import React from 'react';
-import logo from './logo.svg';
+import Form, { UiSchema } from 'react-jsonschema-form';
+import { JSONSchema6 } from "json-schema";
+import getFromPath from 'lodash/get';
+
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface State {
+    configFetched: boolean,
+    config?: {
+        uiSchema: UiSchema,
+        dataSchema: JSONSchema6,
+    },
+};
+
+class App extends React.Component {
+    state: State = {
+        configFetched: false,
+    }
+
+    constructor(props = {}) {
+        super(props);
+
+        this.getConfig();
+    }
+
+    async getConfig() {
+        let config;
+        try {
+            const res = await fetch('/config.json');
+            config = await res.json();
+        } catch (e) {
+            console.error(e);
+
+            return;
+        }
+
+        this.setState({ config });
+    }
+
+    render() {
+        const dataSchema = getFromPath(this.state, 'config.dataSchema');
+        const uiSchema = getFromPath(this.state, 'config.uiSchema');
+
+        return (
+            <div className="App container-fluid">
+                <section>
+                    {
+                        Boolean(dataSchema) && (
+                            <Form
+                                schema={dataSchema}
+                                uiSchema={uiSchema}
+                                onChange={() => console.log('changed')}
+                                onSubmit={() => console.log('submitted')}
+                                onError={() => console.log('errors')}
+                            />
+                        )
+
+                    }
+                </section>
+            </div>
+        );
+    }
 }
 
 export default App;
