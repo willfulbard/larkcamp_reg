06bb6c58fe9651f4e89a95fbb78e5efc9a3a39eb
Evin Sellin <evinism@gmail.com>
2019-10-20 17:24:00 -0700

    modify tests and add in new logic to ensure that pricing is done correctly

--- src/__tests__/config.test.js
+++ src/__tests__/config.test.js
@@ -21,33 +21,33 @@ describe('Pricing Logic', () => {
   // [Payment type, camp duration, meals, age, # parking passes]
   const truthTable = [
     // Discount logic
-    ['Check', 'Full camp', 'None', , 0, 764],
-    ['Paypal', 'Full camp', 'None', , 0, 784],
-    ['Credit Card', 'Full camp', 'None', , 0, 784],
+    ['Check', 'Full camp', 'None', , 0, 76400],
+    ['Paypal', 'Full camp', 'None', , 0, 78400],
+    ['Credit Card', 'Full camp', 'None', , 0, 78400],
   
     // Different ages across different camp types
-    ['Paypal', 'Full camp', 'None', 18, 0, 784],
-    ['Paypal', 'Full camp', 'None', 11, 0, 596],
+    ['Paypal', 'Full camp', 'None', 18, 0, 78400],
+    ['Paypal', 'Full camp', 'None', 11, 0, 59600],
     ['Paypal', 'Full camp', 'None', 3, 0, 0],
-    ['Paypal', 'Half camp (1st half)', 'None', 18, 0, 596],
-    ['Paypal', 'Half camp (1st half)', 'None', 11, 0, 484],
+    ['Paypal', 'Half camp (1st half)', 'None', 18, 0, 59600],
+    ['Paypal', 'Half camp (1st half)', 'None', 11, 0, 48400],
     ['Paypal', 'Half camp (1st half)', 'None', 3, 0, 0],
   
     // Different ages for different meals
-    ['Paypal', 'Full camp', 'Full Camp, All Meals', 18, 0, 784 + 406],
-    ['Paypal', 'Full camp', 'Full Camp, All Meals', 11, 0, 596 + 304],
-    ['Paypal', 'Full camp', 'Full Camp, All Meals', 3, 0, 304],
+    ['Paypal', 'Full camp', 'Full Camp, All Meals', 18, 0, 78400 + 40600],
+    ['Paypal', 'Full camp', 'Full Camp, All Meals', 11, 0, 59600 + 30400],
+    ['Paypal', 'Full camp', 'Full Camp, All Meals', 3, 0, 30400],
 
-    ['Paypal', 'Full camp', 'Full Camp, Just Dinners', 18, 0, 784 + 230],
-    ['Paypal', 'Full camp', 'Full Camp, Just Dinners', 11, 0, 596 + 166],
-    ['Paypal', 'Full camp', 'Full Camp, Just Dinners', 3, 0, 166],
+    ['Paypal', 'Full camp', 'Full Camp, Just Dinners', 18, 0, 78400 + 23000],
+    ['Paypal', 'Full camp', 'Full Camp, Just Dinners', 11, 0, 59600 + 16600],
+    ['Paypal', 'Full camp', 'Full Camp, Just Dinners', 3, 0, 16600],
 
-    ['Paypal', 'Half camp (1st half)', 'Half Camp (first half), All Meals', 18, 0, 596 + 230],
-    ['Paypal', 'Half camp (1st half)', 'Half Camp (first half), All Meals', 11, 0, 484 + 166],
-    ['Paypal', 'Half camp (1st half)', 'Half Camp (first half), All Meals', 3, 0, 166],
+    ['Paypal', 'Half camp (1st half)', 'Half Camp (first half), All Meals', 18, 0, 59600 + 23000],
+    ['Paypal', 'Half camp (1st half)', 'Half Camp (first half), All Meals', 11, 0, 48400 + 16600],
+    ['Paypal', 'Half camp (1st half)', 'Half Camp (first half), All Meals', 3, 0, 16600],
   
     // Parking Pass
-    ['Paypal', 'Full camp', 'None', 18, 1, 784 + 62],
+    ['Paypal', 'Full camp', 'None', 18, 1, 78400 + 6200],
   ];
 
   truthTable.forEach((entry) => {
@@ -68,6 +68,34 @@ describe('Pricing Logic', () => {
         parking_passes: Array(numPasses).fill({}),
       })).toEqual(price);
     });
+  });
+
+  it('correctly handles non-integer values for t-shirts', () => {
+    const pricingLogic = JSON.parse(config).pricingLogic;
+    expect(jsonLogic.apply(pricingLogic, {
+      tshirt_sizes: {
+        small: 1.05,
+        medium: 1.05,
+        large: 1.05,
+        xl: 1.05,
+        xxl: 1.05,
+      },
+      sweatshirt_sizes: {
+        small: 1.05,
+        medium: 1.05,
+        large: 1.05,
+        xl: 1.05,
+        xxl: 1.05,
+      }
+    })).toEqual(18400);
+  });
+
+  it('correctly handles non-integer values for donations', () => {
+    const pricingLogic = JSON.parse(config).pricingLogic;
+    expect(jsonLogic.apply(pricingLogic, {
+      lta_donation: 1.0540,
+      woodlands_donation: 1.4390,
+    })).toEqual(248);
   });
 });
 
